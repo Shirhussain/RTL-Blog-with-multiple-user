9ea54783124ae7d9037b1bf05544ff8f7a731f83
Hu3in <sh.danishyar@gmail.com>
2020-07-21 20:28:39 +0430

    Category Class Added

--- blog/admin.py
+++ blog/admin.py
@@ -1,13 +1,27 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Category
 
-
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('title', 'position')
+    #by default we can't use boolean field here for that reason i have to 
+    # i have to change it to list
+    list_filter = (['status'])
+    search_fields = ('title',)
+    prepopulated_fields = {'slug': ('title',)}
+    
+    
+    
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', "jpublish", "status",)
+    #here i cant't use ManyToManyField like category, i have to change it to sring first 
+    #for this reason i will define a function as follows
+    list_display = ('title', "jpublish", "status",'category_to_str')
     list_filter = ('publish', 'status')
     search_fields = ('title','description')
     prepopulated_fields = {'slug': ('title',)}
     ordering = ['status','-publish']
 
-# admin.site.register(Article)
+    def category_to_str(self, obj):
+        return "Categories"
+# admin.site.register(Article, ArticleAdmin)
--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,21 @@ from django.utils.text import slugify
 
 from extensions.utils import jalali_converter
 
+class Category(models.Model):
+    title = models.CharField(max_length=200, verbose_name='عنوان دسته بندی')
+    slug = models.SlugField(max_length=200, allow_unicode=True, unique=True, verbose_name='آدرس دسته بندی')
+    status = models.BooleanField(default=True, verbose_name='آیا نمایش داده شود؟')
+    position = models.IntegerField(verbose_name='مقام')
+
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+        ordering = ['position']
+
+    def __str__(self):
+        return self.title
+
+
 class Article(models.Model):
     STATUS_CHOIICES = (
         ('d', 'پیش نویس'),
@@ -15,6 +30,7 @@ class Article(models.Model):
     # اگر بخوااهیم به زبان فارسی سلگ را در یوارال استفاده کنیم ، بخاطر این کار باید از الو یونی کد استفاده کنیم
     # تنها این کار کافی نیست باید در قسمت یوآرال هم از رجیکس ها استفاده کنیم زیرا پت کارساز نیست
     slug = models.SlugField(max_length=100, allow_unicode=True, unique=True, verbose_name='آدرس') 
+    category = models.ManyToManyField(Category, verbose_name='دسته بندی')
     description = models.TextField(verbose_name='توضیحات')
     thumbnail = models.ImageField(upload_to="thumbnials", verbose_name='تصویر')
     publish = models.DateTimeField(default=timezone.now, verbose_name='زمان انتشار')
